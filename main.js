94ed2cf027d30b43cc58e71d66a219d0477a8a03
Mesi21 <mse212002@gmail.com>
2021-05-24 18:17:59 +0300

    :rotating_light: fixed linter warnings

--- main.js
+++ main.js
@@ -6,49 +6,49 @@ const createGrid = (cols, rows) => {
   board.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
 
-  let s = cols * rows;
+  const s = cols * rows;
 
-  for (i = 0; i < s; i++) {
-      let item = document.createElement('div');
-      item.classList.add('grid-item');
-      board.append(item);
-  };
+  for (let i = 0; i < s; i++) {
+    let item = document.createElement('div');
+    item.classList.add('grid-item');
+    board.append(item);
+  }
   
-  gridElements = document.querySelectorAll('.grid-item');
+  let gridElements = document.querySelectorAll('.grid-item');
   gridElements.forEach((item) => {
-      item.addEventListener("mouseover", (ev) => {
-          changeColor(ev);
+      item.addEventListener('mouseover', (ev) => {
+        changeColor(ev);
       });
   });
-}
+};
 
 const changeColor = (ev) => {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
   ev.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-  return ev
-}
+  return ev;
+};
 
 const reset = () => {
-  resetBoard.addEventListener("click", (ev) => {
-      gridElements.forEach((item) => {
-          item.style = null;
-      });
+  resetBoard.addEventListener('click', () => {
+    gridElements.forEach((item) => {
+        item.style = null;
+    });
   });
-}
+};
 
 
-changeBoard.addEventListener("click", (ev) => {
-    cols = prompt("How many cols?");
-    rows = prompt("How many rows?");
-    createGrid(cols, rows);
-})
+changeBoard.addEventListener('click', () => {
+  cols = prompt('How many cols?');
+  rows = prompt('How many rows?');
+  createGrid(cols, rows);
+});
 
 const starter = () => {
-    createGrid();
-    reset();
-}
+  createGrid();
+  reset();
+};
 
 starter();
 
