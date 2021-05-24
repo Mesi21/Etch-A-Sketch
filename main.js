33c660a70cf18ce22b02febae7cf177c076920a0
Mesi21 <mse212002@gmail.com>
2021-05-24 18:13:04 +0300

    :rotating_light: fixed linter warnings

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
-const board = document.getElementById("board");
-const resetBoard = document.getElementById("reset_btn");
-const changeBoard = document.getElementById("grid_size");
+const board = document.getElementById('board');
+const resetBoard = document.getElementById('reset_btn');
+const changeBoard = document.getElementById('grid_size');
 
 const createGrid = (cols, rows) => {
-    board.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
-    board.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+  board.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
 
-    let s = cols * rows;
+  let s = cols * rows;
 
-    for (i = 0; i < s; i++) {
-        let item = document.createElement("div");
-        item.classList.add("grid-item");
-        board.append(item);
-    };
-    
-    gridElements = document.querySelectorAll(".grid-item");
-    gridElements.forEach((item) => {
-        item.addEventListener("mouseover", (ev) => {
-            changeColor(ev);
-        });
-    });
+  for (i = 0; i < s; i++) {
+      let item = document.createElement('div');
+      item.classList.add('grid-item');
+      board.append(item);
+  };
+  
+  gridElements = document.querySelectorAll('.grid-item');
+  gridElements.forEach((item) => {
+      item.addEventListener("mouseover", (ev) => {
+          changeColor(ev);
+      });
+  });
 }
 
 const changeColor = (ev) => {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-    ev.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    return ev
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  ev.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  return ev
 }
 
 const reset = () => {
-    resetBoard.addEventListener("click", (ev) => {
-        gridElements.forEach((item) => {
-            item.style = null;
-        });
-    });
+  resetBoard.addEventListener("click", (ev) => {
+      gridElements.forEach((item) => {
+          item.style = null;
+      });
+  });
 }
 
 
